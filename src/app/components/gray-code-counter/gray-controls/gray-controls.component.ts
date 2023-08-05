5af4bd2e304d43615f7be4543a4a82be7bb50523
MaxenV <maxen2001@gmail.com>
2023-08-05 16:12:02 +0200

    Add validatots to gray-controls

--- src/app/components/gray-code-counter/gray-controls/gray-controls.component.ts
+++ src/app/components/gray-code-counter/gray-controls/gray-controls.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { GrayCodeCounterService } from '../gray-code-counter.service';
 
 @Component({
@@ -10,9 +10,9 @@ import { GrayCodeCounterService } from '../gray-code-counter.service';
 export class GrayControlsComponent {
 
   controlsSettings: FormGroup = new FormGroup({
-    'animationSpeed': new FormControl(1000),
-    'destinyNumber': new FormControl(10),
-    'startNumber': new FormControl(0)
+    'animationSpeed': new FormControl(-1000, this.correctNumberValidator),
+    'destinyNumber': new FormControl(10, this.correctNumberValidator),
+    'startNumber': new FormControl(0, this.correctNumberValidator)
   });
 
   constructor(private counterService: GrayCodeCounterService) { }
@@ -27,4 +27,11 @@ export class GrayControlsComponent {
     )
   }
 
+  correctNumberValidator(control: AbstractControl): ValidationErrors | null {
+    if (control.value === '') return { required: true }
+    if (+control.value < 0) return { negativeNumber: true }
+    if ((+control.value) % 1 !== 0) return { floatingNumber: true }
+    return null;
+  }
 }
+
